9d5fadabdc2f18681520be9e86e92da277c1acd8
jayse45 <ngyasi0@gmail.com>
2022-04-24 20:59:54 +0000

    template

---
+++ restaurant/src/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <nav>
+        <h2>Logo</h2>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Produv</li>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default navbar
